f88185c672a21dfac7ba4c8e9fa32e8964907704
gaillers <gailers@mail.ru>
2021-10-25 00:04:54 +0300

    10

--- Hillel - ДЗ 10. TODO List/script.js
+++ Hillel - ДЗ 10. TODO List/script.js
@@ -1,26 +1,24 @@
 (() => {
-   const btnAdd = document.getElementById('button-add');
-   const deskTaskInput = document.getElementById('text-todo');
-   const todoList = document.querySelector('.todo-list');  
-   
+  const btnAdd = document.getElementById('btn-add');
+  const deskTaskInput = document.getElementById('text-todo');
+  const todoList = document.querySelector('.todo-list');
 
-   btnAdd.addEventListener('click', () => {
-     const newTask = document.createElement('div');
-     newTask.innerHTML = `
+  btnAdd.addEventListener('click', (index) => {
+    const deskTaskContent = deskTaskInput.value;
+    if (!deskTaskContent) return;
+
+    const newTask = document.createElement('div');
+    newTask.innerHTML = `
      <div class="todo-item">
-        <div class="description">${deskTaskInput.value}</div>
+        <div class="description">${deskTaskContent}</div>
+        <div class="buttons">
+          <input onclick="completeTask(${index})" id="btn-complate" class="btn-complate" type="checkbox">
+          <button onclick="deleteTask(${index})" id="btn-delete" class="btn-delete">Delete</button>
+        </div>
       </div>
     `;
-    
-     deskTaskInput.value = '';
-     todoList.prepend(newTask);
-   });
 
+    deskTaskInput.value = '';
+    todoList.prepend(newTask);
+  });
 })();
-
-
-
-{/* <div class="buttons">
-          <input id="btn-complate" class="btn-complate" type="checkbox">
-          <button id="btn-delete" class="btn-delete">Delete</button>
-        </div> */}
