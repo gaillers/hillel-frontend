105e81a57c3267e766c979a6eda1b87d13635347
gaillers <gailers@mail.ru>
2021-10-25 23:43:45 +0300

    finish 10

--- Hillel - ДЗ 10. TODO List/script.js
+++ Hillel - ДЗ 10. TODO List/script.js
@@ -3,22 +3,33 @@
   const deskTaskInput = document.getElementById('text-todo');
   const todoList = document.querySelector('.todo-list');
 
-  btnAdd.addEventListener('click', (index) => {
+  btnAdd.addEventListener('click', () => {
     const deskTaskContent = deskTaskInput.value;
     if (!deskTaskContent) return;
 
     const newTask = document.createElement('div');
+    newTask.classList.add('todo-item');
     newTask.innerHTML = `
-     <div class="todo-item">
-        <div class="description">${deskTaskContent}</div>
-        <div class="buttons">
-          <input onclick="completeTask(${index})" id="btn-complate" class="btn-complate" type="checkbox">
-          <button onclick="deleteTask(${index})" id="btn-delete" class="btn-delete">Delete</button>
-        </div>
-      </div>
-    `;
+      <div class="description">${deskTaskContent}</div>
+      <div class="buttons">
+          <input id="btn-complate" class="btn-complate" type="checkbox">
+        <button id="btn-remove" class="btn-remove">Delete</button>
+       </div>
+      `;
 
     deskTaskInput.value = '';
     todoList.prepend(newTask);
+
+    const complateTask = document.getElementById('btn-complate');
+    complateTask.addEventListener('change', () => {
+      newTask.classList.toggle('checked');
+    });
+
+    const removeTask = document.getElementById('btn-remove');
+    removeTask.addEventListener('click', () => {
+      newTask.remove();
+    });
+
   });
+  
 })();
